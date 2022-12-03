19e47693260e23c0314abff86993777e4789d902
Kehinde Onifade <onifkay@gmail.com>
2022-12-03 12:35:39 +0100

    add x86_64-linux

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_233930) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_113359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "students", force: :cascade do |t|
     t.string "name"
-    t.date "dob"
+    t.string "dob"
+    t.integer "age"
+    t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
