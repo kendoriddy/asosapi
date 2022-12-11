a6cf8073bae15c10c0e9ea4a0622d6737445cda1
Kehinde Onifade <onifkay@gmail.com>
2022-12-11 14:12:14 +0100

    add gender to students db

--- db/migrate/20221127233930_create_students.rb
+++ db/migrate/20221127233930_create_students.rb
@@ -2,6 +2,7 @@ class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
       t.string :name
+      t.string :gender
       t.date :dob
 
       t.timestamps
