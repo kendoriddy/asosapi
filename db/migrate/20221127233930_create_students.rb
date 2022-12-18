f71c21665d14dbebe175e11b0f416c4ed76a7641
Kehinde Onifade <onifkay@gmail.com>
2022-12-18 16:38:21 +0100

    update student controller

--- db/migrate/20221127233930_create_students.rb
+++ db/migrate/20221127233930_create_students.rb
@@ -2,7 +2,6 @@ class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :gender
       t.date :dob
 
       t.timestamps
